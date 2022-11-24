f075e0d9a2bbb38ff0cf420646e07944e662a218
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-24 11:43:07 +0100

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    project: "tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier", "jsx-a11y"],
+  rules: {
+    "unicorn/prefer-top-level-await": "none",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        singleQuote: true,
+        jsxSingleQuote: true,
+        semi: false,
+        trailingComma: "none",
+      },
+    ],
+    "jsx-a11y/rule-name": 2,
+  },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# React Heroes SPA
+
+SPA of Heroes for viewing information about Marvel and DC Heroes. You can view heroes filtering by brand, search heroes and view the details of one heroe.
