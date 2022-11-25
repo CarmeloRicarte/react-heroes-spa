d9dbfafda643bd3b2c357d85c62555e40671a024
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-25 15:58:47 +0100

    perf: add useMemo for get heroes

--- src/heroes/components/HeroList.tsx
+++ src/heroes/components/HeroList.tsx
@@ -1,10 +1,10 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
 import { getHeroesByPublisher } from "../helpers";
 import { HeroCard } from "./HeroCard";
 
 export const HeroList: FC<{ publisher: string }> = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
     <>
       <div className="row row-cols-1 row-cols-md-3 g-3">
