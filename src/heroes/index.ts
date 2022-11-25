0d8b5c0b94258d5fd039adba33a6fc4ea1afd471
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-25 11:55:00 +0100

    chore: update barrel file

--- src/heroes/index.ts
+++ src/heroes/index.ts
@@ -1,3 +1,6 @@
+export * from "./components";
 export * from "./data/heroes";
+export * from "./helpers";
+export * from "./models";
 export * from "./pages";
 export * from "./routes/HeroesRoutes";
