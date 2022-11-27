999562d42a04d4caaa0316b96935874109289539
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-27 18:01:24 +0100

    feat: remember last page visited

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -7,8 +7,9 @@ export const LoginPage = () => {
   const { login } = useContext(AuthContext);
 
   const onLogin = () => {
+    const lastPath = localStorage.getItem("lastPath") || "/";
     login("Carmelo RR");
-    navigate("/", {
+    navigate(lastPath, {
       replace: true,
     });
   };
--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../auth";
 
 interface PrivateRouteProps {
@@ -8,6 +8,14 @@ interface PrivateRouteProps {
 export const PrivateRoute = ({ children }: PrivateRouteProps) => {
   const { authState } = useContext(AuthContext);
   const { logged } = authState;
+
+  const { pathname, search } = useLocation();
+  const lastPath = pathname + search;
+
+  useEffect(() => {
+    localStorage.setItem("lastPath", lastPath);
+  }, [pathname, search]);
+
   // if the user is logged in, then navigates to children routes. If not, navigates to login route
   return logged ? children : <Navigate to="/login" />;
 };
